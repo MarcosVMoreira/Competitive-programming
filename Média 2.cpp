88f988a814a76680900c825eb7daa54e06bd2cf0
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2018-09-28 00:16:29 -0300

    Inicio

---
+++ Média 2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    double a, b, c, result;
+    
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    result = (a*2+b*3+c*5)/10;
+    
+    printf("MEDIA = %.1lf", result);
+
+    printf("\n");
+
+    return 0;
+}
+
---
+++ SalarioComBonus.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+// Nome de Tipos
+typedef long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+// Valores
+#define INF 0x3F3F3F3F
+#define LINF 0x3F3F3F3F3F3F3F3FLL
+#define DINF (double)1e+30
+#define EPS (double)1e-9
+#define RAD(x) (double)(x*PI)/180.0
+#define PCT(x,y) (double)x*100.0/y
+
+// Atalhos
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define l length()
+#define forn(i, n) for ( int i = 0; i < (n); ++i )
+#define fornx(i, x, n) for ( int i = (x); i < (n); ++i )
+
+using namespace std;
+int main(){
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout); 
+
+    string nome;
+    double salario, vendas;
+
+
+
+    cin >> nome;
+
+    scanf ("%lf %lf", &salario, &vendas);
+
+    salario = (salario + vendas*0.15);
+
+    printf("TOTAL = R$ %.2lf", salario);
+
+
+    printf("\n");
+
+    return 0;
+}
+
